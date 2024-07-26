3b87e503bcd2ab0160ba3c637b456f2da731f576
victorgallegosc <victorgc11@outlook.com>
2024-07-25 18:16:02 -0600

    Added Projects section

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
+  projects = [
+    {
+      title: 'Personal Portfolio',
+      link: 'www.victorgallegos.com',
+      image: 'img/Profile-skin.webp',
+      repo: 'https://github.com/victorgallegosc/portfolio2.0',
+      description: 'This website was constructed in Angular and based on a prototype designed by me using Figma.',
+      skills: [
+        'HTML',
+        'CSS',
+        'Angular',
+        'Git',
+        'Github',
+        'UI/UX',
+        'Figma'
+      ]
+    }
+  ]
 
+  goToRepo(link: string) {
+    window.open(link, '_blank');
+  }
 }
